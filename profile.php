1625cecd964bafd5ceb335a89e3556e0a4e234b7
Ramadhangalang <ramadhangalang1702@gmail.com>
2023-05-31 13:39:33 +0800

    pertemuan 3

---
+++ profile.php
@@ -0,0 +1,54 @@
+<?php  ?>
+<div class="row">
+    <div class="col-md-12">
+        <div class="text-center">
+            <img src="img/galang.png" alt="Ramdhan Galang Gandewa" width="100" class="rounded-circle">
+            <table class="table mt-4" style="width:100%; margin:0 auto;">
+                <tbody>
+                    <tr>
+                        <td align="right" width="50%"><strong>NPM :</strong></td>
+                        <td align="left" width="50%">2110010135</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Fakultas / Program Studio :</strong></td>
+                        <td align="left" width="50%">Teknologi Informasi / Teknik Informatika</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Nama Lengkap :</strong></td>
+                        <td align="left" width="50%">Ramadhan Galang Gandewa</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Tempat, Tanggal Lahir :</strong></td>
+                        <td align="left" width="50%">Kota Madiun 17 November 2023</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Telepon :</strong></td>
+                        <td align="left" width="50%">0853 4888 8888</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>email :</strong></td>
+                        <td align="left" width="50%">ramadhangalang1702@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Media Sosial :</strong></td>
+                        <td align="left" width="50%">
+                            <a href="http://facebook.com/" target="_blank" style="text-decoration:none;">
+                                <img src="img/facebook.svg" width="30" alt="facebook">
+                            </a>
+                            <a href="http://instagram.com/" target="_blank" style="text-decoration:none;">
+                                <img src="img/instagram.svg" width="30" alt="instagram">
+                            </a>
+                            <a href="http://twitter.com/" target="_blank" style="text-decoration:none;">
+                                <img src="img/twitter.svg" width="30" alt="twitter">
+                            </a>
+                            <a href="http://linkedin.com/in/" target="_blank">
+                                <img src="img/linkedin.svg" width="30" alt="linkedin">
+                            </a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<?php include "footer.php" ?>
